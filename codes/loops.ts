cf8cc97ed7f8cd87ffcc08e3872b6532dd8e60d9
Moiz Ahmed <93610141+moizahmedtuc@users.noreply.github.com>
2023-09-16 23:42:15 +0200

    Create loops.ts

---
+++ codes/loops.ts
@@ -0,0 +1,14 @@
+function countSpecificAlphabet(input: string, targetAlphabet: string): number {
+    let count = 0;
+    for (let i = 0; i < input.length; i++) {
+        if (input[i] === targetAlphabet) {
+            count++;
+        }
+    }
+    return count;
+}
+
+const inputString = "hello world";
+const target = "o";
+const result = countSpecificAlphabet(inputString, target);
+console.log(`The alphabet '${target}' appears ${result} times.`);
